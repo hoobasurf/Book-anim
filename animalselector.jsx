f4894f8dd10c280781e1ead94a1060ac7e4b20ec
hoobasurf <brice.paneela@icloud.com>
2025-11-02 16:18:42 +0100

    Update animalselector.jsx

--- animalselector.jsx
+++ animalselector.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const animals = {
-  Savane: ["Lion"],
-  Océan: ["Dauphin"],
-  Ferme: ["Vache"],
-  Forêt: ["Renard"],
-};
+function AnimalSelector({ environment, setAnimal }) {
+  const animals = {
+    Savane: ["Lion"],
+    Océan: ["Dauphin"],
+    Ferme: ["Vache"],
+    Forêt: ["Renard"],
+  };
 
-export default function AnimalSelector({ environment, setAnimal }) {
   return (
     <div>
       <h2>Choisis un animal dans {environment} :</h2>
@@ -19,3 +19,5 @@ export default function AnimalSelector({ environment, setAnimal }) {
     </div>
   );
 }
+
+export default AnimalSelector;
