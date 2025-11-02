9e631e9121a6b9d680b6dd40eab4eb10328f8638
hoobasurf <brice.paneela@icloud.com>
2025-11-02 16:19:58 +0100

    Update animation.jsx

--- animation.jsx
+++ animation.jsx
@@ -1,1 +1,20 @@
+import React from "react";
 
+function Animation({ environment, animal, onBack }) {
+  return (
+    <div>
+      <h2>Animation du {animal}</h2>
+      <video
+        src={`${environment.toLowerCase()}_${animal.toLowerCase()}.mp4`}
+        autoPlay
+        loop
+        controls
+        style={{ maxWidth: "100%", borderRadius: "10px" }}
+      />
+      <br />
+      <button onClick={onBack}>Retour au coloriage</button>
+    </div>
+  );
+}
+
+export default Animation;
